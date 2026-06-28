61f879cf97cc21117ed590ce0f9bdb6283ef3205
adeelkhan-devops <adeel.khanisb@gmail.com>
2026-06-28 19:58:15 +0500

    app.js ( port fixing

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const server = http.createServer((req, res) => {
   res.end('Hello DevOps!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
